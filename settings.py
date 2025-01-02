e0a256cc5fb3b095f7b58fe48984df47643c9b3a
Sonia Sherif <soniasherif@Sonias-MBP.lan>
2025-01-02 03:58:20 -0600

    Added the alien class.

--- settings.py
+++ settings.py
@@ -5,10 +5,10 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 1920
-        self.screen_height = 1080
-        #self.bg_color = (156, 46, 53)
-        self.bg = pygame.image.load("images/Space_Background1.png")
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (156, 46, 53)
+        #self.bg = pygame.image.load("images/Space_Background1.png")
         self.ship_speed = 2.0
         # Bullet settings
         self.bullet_speed = 3.0
